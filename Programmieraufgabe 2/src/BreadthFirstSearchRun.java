188a203b60278bc5e2f226a731a7c4a431270bd8
Martin Fleischer <fleischer.martin@gmx.net>
2012-05-04 13:54:15 +0200

    test

--- Programmieraufgabe 2/src/BreadthFirstSearchRun.java
+++ Programmieraufgabe 2/src/BreadthFirstSearchRun.java
@@ -12,10 +12,10 @@ public class BreadthFirstSearchRun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Graph<Vertex, Edge<Vertex>> graph8Directed = GraphLesen.FileToGraph("/home/martin/workspace/Programmieraufgabe 2/src/graphBeispiele/graph8.txt",true);
-		Graph<Vertex, Edge<Vertex>> graph9Directed = GraphLesen.FileToGraph("/home/martin/workspace/Programmieraufgabe 2/src/graphBeispiele/graph9.txt",true);
-		Graph<Vertex, Edge<Vertex>> graph20Directed = GraphLesen.FileToGraph("/home/martin/workspace/Programmieraufgabe 2/src/graphBeispiele/graph20.txt",true);
-		Graph<Vertex, Edge<Vertex>> graph20Undirected = GraphLesen.FileToGraph("/home/martin/workspace/Programmieraufgabe 2/src/graphBeispiele/graph20.txt",false);
+		Graph<Vertex, Edge<Vertex>> graph8Directed = GraphLesen.FileToGraph("/home/martin/workspace/Algorithmen/Programmieraufgabe 2/src/graphBeispiele/graph8.txt",true);
+		Graph<Vertex, Edge<Vertex>> graph9Directed = GraphLesen.FileToGraph("/home/martin/workspace/Algorithmen/Programmieraufgabe 2/src/graphBeispiele/graph9.txt",true);
+		Graph<Vertex, Edge<Vertex>> graph20Directed = GraphLesen.FileToGraph("/home/martin/workspace/Algorithmen/Programmieraufgabe 2/src/graphBeispiele/graph20.txt",true);
+		Graph<Vertex, Edge<Vertex>> graph20Undirected = GraphLesen.FileToGraph("/home/martin/workspace/Algorithmen/Programmieraufgabe 2/src/graphBeispiele/graph20.txt",false);
 		System.out.println(graph8Directed.toString());
 	}
 
