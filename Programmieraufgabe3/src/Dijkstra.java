6bd42a7efe829aca6e6aa5ac2ac0ad250404cf29
abuntu <aboud@aboud-Aspire-5739G.(none)>
2012-05-30 14:10:32 +0200

    last version Dijkstra algorithem

--- Programmieraufgabe3/src/Dijkstra.java
+++ Programmieraufgabe3/src/Dijkstra.java
@@ -77,7 +77,7 @@ public class Dijkstra {
 
 			Vertex lowestElementInQueue = queue.get(lowest);
             
-			//stop the loop if the destinatiopn is reached
+			//stop the loop if the destination is reached
 			if ( lowestElementInQueue.getId() == destinationVertexID)break;
 			
 			queue.remove(lowestElementInQueue);
